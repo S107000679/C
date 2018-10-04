bfcbac00c220359812ca1da4c5863adfad9a4134
S107000679 <43431947+S107000679@users.noreply.github.com>
2018-10-04 15:07:05 +0800

    20181004C++

---
+++ C15.cpp
@@ -0,0 +1,15 @@
+/* prog3_6, 字元型態的列印問題*/
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+	printf("恩師好 我是陳冠喨\n");
+	printf("我的C300之15\n");
+   int i=298;
+   printf("ASCII(%d)=%c\n",i,i);	/* 印出ASCII碼為i的字元 */
+   
+   system("pause");
+   return 0;
+}
+
+
