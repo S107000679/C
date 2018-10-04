99b566e964232cd5f612c18c635f5ca1e3a34a54
S107000679 <43431947+S107000679@users.noreply.github.com>
2018-10-04 15:17:52 +0800

    Add files via upload

---
+++ 20181004_C300/C10.cpp
@@ -0,0 +1,17 @@
+/* prog3_1,¶]?úY£~? */
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+	printf("®¦®v¦n §Ú¬O³¯«aØg\n");
+	printf("§ÚªºC300¤§10\n");
+   int num1=12400;	  /* úä”Ònum1ûn­Ñ?¶]?Ş•Ñ?üGûn12400 */
+   double num2=5.234;	  /* úä”Ònum2ûnûı¥°úöÅ|íå?¶]?Ş•Ñ?üGûn5.234 */
+   
+   printf("%d is an integer\n",num1); 	/* ¡À“¹printf()£¸? */
+   printf("%f is a double\n",num2);	/* ¡À“¹printf()£¸? */
+
+   system("pause");
+   return 0;
+}
+
---
+++ 20181004_C300/C11.cpp
@@ -0,0 +1,19 @@
+/* prog3_2, 短整數資料型態的溢位*/
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+	printf("恩師好 我是陳冠喨\n");
+	printf("我的C300之11\n");
+   short sum,s=32767;        /* 宣告短整數變數sum與s */
+   
+   sum=s+1;
+   printf("s+1= %d\n",sum);	/* 列印出sum的值 */
+   
+   sum=s+2;
+   printf("s+2= %d\n",sum); 	/* 列印出sum的值 */   
+
+   system("pause");
+   return 0;
+}
+
